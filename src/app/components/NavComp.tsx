79cca3599a8c81977109758952275c6e6134912e
zachdressing <zed_dressel@hotmail.com>
2024-04-29 11:36:48 -0700

    Initial, NavBar

---
+++ src/app/components/NavComp.tsx
@@ -0,0 +1,34 @@
+"use client";
+import {Dropdown, Navbar } from 'flowbite-react'
+
+const NavComp = () => {
+  return (
+    <div>
+      <Navbar fluid className='w-screen h-28 bg-navBack'>
+        <Navbar.Brand>
+          <img src='/Logo.png' alt='maddie is cool!'/>
+        </Navbar.Brand>
+        <div className="flex md:order-2">
+          <Dropdown
+            className='border border-black text-center'
+            arrowIcon={false}
+            inline
+            label={
+              <div className='w-16 h-16 rounded-full bg-blueish border border-black'></div>
+            }
+          >
+            <Dropdown.Item>PROFILE</Dropdown.Item>
+            <Dropdown.Item>CREATE BOARD</Dropdown.Item>
+            <Dropdown.Item>LOG OUT<img className='ml-4' src='/Vector.png' alt='vector'/></Dropdown.Item>
+          </Dropdown>
+          <Navbar.Toggle />
+        </div>
+        <Navbar.Collapse>
+
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
+  )
+}
+
+export default NavComp
---
+++ tailwind.config.ts
@@ -0,0 +1,31 @@
+const flowbite = require("flowbite-react/tailwind");
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "node_modules/flowbite-react/lib/esm/**/*.js",
+    flowbite.content(),
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+          'navBack': 'linear-gradient(90deg, #AEE6D9 0%, #3EBE9F 100%)',
+      },
+      colors: {
+        "blueish": "rgba(87, 205, 255, 1)",
+        "reddish": "rgb(255,110,110)",
+        "purplish": "rgb(213,159,255)",
+      },
+    },
+  },
+  plugins: [
+    flowbite.plugin(),
+  ],
+};
+export default config;
